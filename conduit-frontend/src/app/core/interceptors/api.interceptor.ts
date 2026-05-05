6fde3b2570a40d2e8b33c1e330bc0cc62b1f5fb9
okan <okan.uzal@mauzer-it.com>
2026-05-05 23:00:32 +0200

    fix api error

--- conduit-frontend/src/app/core/interceptors/api.interceptor.ts
+++ conduit-frontend/src/app/core/interceptors/api.interceptor.ts
@@ -10,11 +10,12 @@ import { Observable } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class ApiInterceptor implements HttpInterceptor {
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    const apiReq = req.clone({ url: `${environment.apiUrl}${req.url}` });
-    return next.handle(apiReq);
-  }
+intercept(
+  req: HttpRequest<any>,
+  next: HttpHandler
+): Observable<HttpEvent<any>> {
+  const normalizedUrl = req.url.endsWith("/") ? req.url.slice(0, -1) : req.url;
+  const apiReq = req.clone({ url: `${environment.apiUrl}${normalizedUrl}` });
+  return next.handle(apiReq);
+}
 }

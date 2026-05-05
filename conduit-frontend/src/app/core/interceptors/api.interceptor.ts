de525b03481fd0e0dbede9529a71f233b1f1eec2
okan <okan.uzal@mauzer-it.com>
2026-05-05 23:11:12 +0200

    fix api

--- conduit-frontend/src/app/core/interceptors/api.interceptor.ts
+++ conduit-frontend/src/app/core/interceptors/api.interceptor.ts
@@ -10,12 +10,27 @@ import { Observable } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class ApiInterceptor implements HttpInterceptor {
-intercept(
-  req: HttpRequest<any>,
-  next: HttpHandler
-): Observable<HttpEvent<any>> {
-  const normalizedUrl = req.url.endsWith("/") ? req.url.slice(0, -1) : req.url;
-  const apiReq = req.clone({ url: `${environment.apiUrl}${normalizedUrl}` });
-  return next.handle(apiReq);
-}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+
+    let url = req.url;
+
+    //feed endpoint needs trailing slash
+    if (url.startsWith("/articles/feed")) {
+      url = url.replace("/articles/feed", "/articles/feed/");
+    }
+
+    //remove trailing slash (except feed)
+    else if (url.endsWith("/")) {
+      url = url.slice(0, -1);
+    }
+
+    const apiReq = req.clone({
+      url: `${environment.apiUrl}${url}`,
+    });
+
+    return next.handle(apiReq);
+  }
 }
